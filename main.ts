da7c510ee1f935041d3c4d61f4c4e1bce3e24aff
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-03-14 12:19:47 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,55 @@
+//  Q no 22
+let Friends :string[] =["Amir","Adil","Wakeel","Raheel"]
+console.log(Friends[4])
+Friends[3] ="Raheel";
+console.log(Friends[3]);
+
+// Q no 23
+let Car :string ="Corolla";
+console.log("is Car ==='Corolla'? I predict true");
+console.log(Car ==="Corolla");
+console.log("is Car ==='Marsadies'? I predict false");
+console.log(Car ==="Marsadies");
+
+let Color :string ="Green"
+console.log("is Color ==='Green'? I predict true");
+console.log(Color ==="Green");
+console.log("is Color ==='red'? I predict false");
+console.log(Color ==="red");
+
+let Watch :string ="Rado"
+console.log("is Watch ==='Rado'? I predict true");
+console.log(Watch ==="Rado");
+console.log("is Watch ==='Seico5'? I predict false");
+console.log(Watch ==="Seico5");
+
+let Pen :string ="Dux"
+console.log("is Pen ==='Dux'? I predict true");
+console.log(Pen ==="Dux");
+console.log("is Pen ==='Dolor'? I predict false");
+console.log(Pen ==="Dolor");
+
+let Stadium :string ="National Stadium Karachi"
+console.log("is Stadium ==='National Stadium Karachi'? I predict true");
+console.log(Stadium ==="National Stadium Karachi");
+console.log("is Stadium ==='Khazafi Stadium Lahore'? I predict false");
+console.log(Watch ==="Khazafi Stadium Lahore");
+
+//  Q no 24
+console.log("'Safdar'=='Safdar'");
+console.log('safdar' == 'safdar');
+
+console.log("5 == 5");
+console.log(5 == 5);
+
+console.log("true == true");
+console.log(true == true);
+
+console.log('is 10 > 8?');
+console.log(10 > 8);
+
+console.log('is 10 < 8?');
+console.log(10 < 8);
+
+console.log(true && false);
+console.log(true || false);
